603d25061100a3944eb43739881aa6f5253379aa
Blake <190663855@qq.com>
2021-02-27 13:34:31 +0800

    添加项目文件。

---
+++ Blake.NET5Project/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Blake.NET5Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blake.NET5Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Time = DateTime.Now;
+            ViewBag.Info = "NET5 v1";
+            ViewBag.Port = $"{base.Request.Host.Port}";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
